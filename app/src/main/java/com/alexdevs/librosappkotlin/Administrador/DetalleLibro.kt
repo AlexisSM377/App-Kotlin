73fb6d5fcab6a2f01ab938ae8d8eab9d1617532d
AlexisSM377 <al222111377@gmail.com>
2024-06-18 10:47:08 -0600

    Se implemento la funcionalidad para poder ver el detalle del libro

--- app/src/main/java/com/alexdevs/librosappkotlin/Administrador/DetalleLibro.kt
+++ app/src/main/java/com/alexdevs/librosappkotlin/Administrador/DetalleLibro.kt
@@ -6,16 +6,71 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alexdevs.librosappkotlin.R
+import com.alexdevs.librosappkotlin.databinding.ActivityDetalleLibroBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DetalleLibro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalleLibroBinding
+    private var idLibro = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalle_libro)
+        binding = ActivityDetalleLibroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        idLibro = intent.getStringExtra("idLibro")!!
+
+        binding.IbRegresar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        cargarDetalleLibro()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun cargarDetalleLibro() {
+        val ref = FirebaseDatabase.getInstance().getReference("Libros")
+        ref.child(idLibro)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //obetener la informacion del libro a traves del id
+                    val categoria = "${snapshot.child("categoria").value}"
+                    val contadorDescargas = "${snapshot.child("contadorDescargas").value}"
+                    val contadorVistas = "${snapshot.child("contadorVistas").value}"
+                    val descripcion = "${snapshot.child("descripcion").value}"
+                    val tiempo = "${snapshot.child("tiempo").value}"
+                    val titulo = "${snapshot.child("titulo").value}"
+                    val url = "${snapshot.child("url").value}"
+
+                    //Formato de tiempo
+                    val fecha = MisFunciones.formatoTiempo(tiempo.toLong())
+                    //Cargar categoria del libro
+                    MisFunciones.CargarCategoria(categoria, binding.categoriaD)
+                    //Cargar la miniatura del libro,contador de paginas
+                    MisFunciones.CargarPdfUrl("$url", "$titulo", binding.VisualizadorPDF, binding.progressBar, binding.paginasD)
+                    //Cargar tamaño pdf
+                    MisFunciones.CargarSizePDF("$url", "$titulo", binding.sizeD)
+
+                    //Cargar la informacion del libro
+                    binding.tituloDetalleLibro.text = titulo
+                    binding.descripcionD.text = descripcion
+                    binding.vistasD.text = contadorVistas
+                    binding.descargasD.text = contadorDescargas
+                    binding.fechaD.text = fecha
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 }
