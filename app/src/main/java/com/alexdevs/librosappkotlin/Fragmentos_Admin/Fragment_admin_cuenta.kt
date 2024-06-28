211ff92b6cc49549bc1cd956353e2380ddb512b0
AlexisSM377 <al222111377@gmail.com>
2024-06-28 14:12:03 -0600

    Perfil de usuario -- lectura de información

--- app/src/main/java/com/alexdevs/librosappkotlin/Fragmentos_Admin/Fragment_admin_cuenta.kt
+++ app/src/main/java/com/alexdevs/librosappkotlin/Fragmentos_Admin/Fragment_admin_cuenta.kt
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.alexdevs.librosappkotlin.Administrador.MisFunciones
 import com.alexdevs.librosappkotlin.Elegir_rol
 import com.alexdevs.librosappkotlin.R
 import com.alexdevs.librosappkotlin.databinding.FragmentAdminCuentaBinding
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class Fragment_admin_cuenta : Fragment() {
@@ -34,10 +41,55 @@ class Fragment_admin_cuenta : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
+
+        cargarInformacion()
+
         binding.CerrarSesionAdmin.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(context, Elegir_rol::class.java))
             activity?.finishAffinity()
         }
+    }
+
+    private fun cargarInformacion() {
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    // Obtener los datos del usuario
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val email = "${snapshot.child("email").value}"
+                    var t_registro = "${snapshot.child("tiempo_registro").value}"
+                    val rol = "${snapshot.child("rol").value}"
+                    val imagen = "${snapshot.child("imagen").value}"
+
+                    if (t_registro == "null") {
+                        t_registro = "0"
+                    }
+
+                    //Convertir a formato de fecha
+                    val formato_fecha = MisFunciones.formatoTiempo(t_registro.toLong())
+
+                    //Setear la información
+                    binding.TxtNombreAdmin.text = nombres
+                    binding.TxtEmailAdmin.text = email
+                    binding.TxtTiempoRegistroAdmin.text = formato_fecha
+                    binding.TxtRolAdmin.text = rol
+
+                    //Setear la imagen
+                    try {
+                        Glide.with(mContext)
+                            .load(imagen)
+                            .placeholder(R.drawable.ic_perfil)
+                            .into(binding.imgPerfil)
+                    } catch (e: Exception) {
+                        Toast.makeText(mContext, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 }
