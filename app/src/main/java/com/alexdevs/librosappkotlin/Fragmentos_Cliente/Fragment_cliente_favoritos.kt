1f238aac692ab8fef0f3161ba5092beb9e225cd7
AlexisSM377 <al222111377@gmail.com>
2024-07-02 00:14:38 -0600

    Dashboard Bottom Navigation Cliente

---
+++ app/src/main/java/com/alexdevs/librosappkotlin/Fragmentos_Cliente/Fragment_cliente_favoritos.kt
@@ -0,0 +1,20 @@
+package com.alexdevs.librosappkotlin.Fragmentos_Cliente
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alexdevs.librosappkotlin.R
+
+class Fragment_cliente_favoritos : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cliente_favoritos, container, false)
+    }
+
+}
--- app/src/main/java/com/alexdevs/librosappkotlin/MainActivityCliente.kt
+++ app/src/main/java/com/alexdevs/librosappkotlin/MainActivityCliente.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alexdevs.librosappkotlin.Fragmentos_Cliente.Fragment_cliente_chat
+import com.alexdevs.librosappkotlin.Fragmentos_Cliente.Fragment_cliente_cuenta
+import com.alexdevs.librosappkotlin.Fragmentos_Cliente.Fragment_cliente_favoritos
 import com.alexdevs.librosappkotlin.databinding.ActivityMainClienteBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -25,6 +28,36 @@ class MainActivityCliente : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         comprobarSesion()
 
+        verFragmentoDashboard()
+
+        binding.BottomNavCliente.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Menu_dashboard_cl -> {
+                    verFragmentoDashboard()
+                    true
+                }
+
+                R.id.Menu_favoritos_cl -> {
+                    verFragmentoFavoritos()
+                    true
+                }
+
+                R.id.Menu_ia_cl -> {
+                    verFragmentoChatIA()
+                    true
+                }
+
+                R.id.Menu_cuenta_cl -> {
+                    verFragmentoCuenta()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -37,8 +70,53 @@ class MainActivityCliente : AppCompatActivity() {
         if (firebaseUser == null) {
             startActivity(Intent(this, Elegir_rol::class.java))
             finishAffinity()
-        }else{
-            Toast.makeText(applicationContext, "Bienvenido(a) ${firebaseUser.email}" , Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Bienvenido(a) ${firebaseUser.email}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
+
+    private fun verFragmentoDashboard(){
+        val nombre_titulo = "Dashboard"
+        binding.TituloRLCliente.text = nombre_titulo
+
+        val fragment = Fragment_cliente_cuenta()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsCliente.id, fragment, "Fragment dashboard")
+        fragmentTransaction.commit()
+
+    }
+
+    private fun verFragmentoFavoritos(){
+        val nombre_titulo = "Favoritos"
+        binding.TituloRLCliente.text = nombre_titulo
+
+        val fragment = Fragment_cliente_favoritos()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsCliente.id, fragment, "Fragment favoritos")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentoChatIA(){
+        val nombre_titulo = "Chat IA"
+        binding.TituloRLCliente.text = nombre_titulo
+
+        val fragment = Fragment_cliente_chat()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsCliente.id, fragment, "Fragment chat")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentoCuenta(){
+        val nombre_titulo = "Cuenta"
+        binding.TituloRLCliente.text = nombre_titulo
+
+        val fragment = Fragment_cliente_cuenta()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsCliente.id, fragment, "Fragment cuenta")
+        fragmentTransaction.commit()
     }
 }
