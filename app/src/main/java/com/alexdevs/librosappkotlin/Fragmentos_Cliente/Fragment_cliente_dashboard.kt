64a24065dd1b33dd73fdf128786fa50172915e31
AlexisSM377 <al222111377@gmail.com>
2024-07-15 12:18:53 -0600

    Listar libros--más vistos y más descargados

--- app/src/main/java/com/alexdevs/librosappkotlin/Fragmentos_Cliente/Fragment_cliente_dashboard.kt
+++ app/src/main/java/com/alexdevs/librosappkotlin/Fragmentos_Cliente/Fragment_cliente_dashboard.kt
@@ -1,6 +1,7 @@
 package com.alexdevs.librosappkotlin.Fragmentos_Cliente
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -11,6 +12,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.alexdevs.librosappkotlin.Administrador.ModeloCategoria
 import com.alexdevs.librosappkotlin.Cliente.AdaptadorCategoria_Cliente
+import com.alexdevs.librosappkotlin.Cliente.TopDescargas
+import com.alexdevs.librosappkotlin.Cliente.TopVistas
 import com.alexdevs.librosappkotlin.R
 import com.alexdevs.librosappkotlin.databinding.FragmentClienteDashboardBinding
 import com.google.firebase.database.DataSnapshot
@@ -59,6 +62,14 @@ class Fragment_cliente_dashboard : Fragment() {
 
             }
         })
+
+        binding.BtnMasVistos.setOnClickListener {
+            startActivity(Intent(mContext, TopVistas::class.java))
+        }
+
+        binding.BtnMasDescargados.setOnClickListener {
+            startActivity(Intent(mContext, TopDescargas::class.java))
+        }
     }
 
     private fun cargarCategorias() {
